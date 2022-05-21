8d273dfceb8214d0c97a0b9fd614a6180b776a33
Jingxuan Feng <henryfeng24@pm.me>
2022-05-22 02:02:10 +1000

    Updated login page

--- Admin Software/src/main/java/rmit/iit/a3/JavaFx/LoginController.java
+++ Admin Software/src/main/java/rmit/iit/a3/JavaFx/LoginController.java
@@ -1,5 +1,6 @@
 package rmit.iit.a3.JavaFx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -11,6 +12,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import rmit.iit.a3.AdminAccount.service.AdminLoginService;
+import rmit.iit.a3.util.SwitchSceneUtil;
 
 @Component
 @FxmlView("Login.fxml")
@@ -36,16 +38,25 @@ public class LoginController {
 
     public void signIn(){
         if(adminLoginService.login(email.getText(), password.getText(), errorLabel)){
+            System.err.println("Successful login!");
             //TODO: Take user to main page
         }
     }
 
-    public void forgotPassword(){
-
+    public void forgotPassword(ActionEvent event){
+        try{
+            SwitchSceneUtil.switchFXML(event, fxWeaver.loadView(ForgotPasswordController.class));
+        }catch (Exception e){
+            logger.error(e.getMessage());
+        }
     }
 
-    public void register(){
-
+    public void register(ActionEvent event){
+        try{
+            SwitchSceneUtil.switchFXML(event, fxWeaver.loadView(RegisterController.class));
+        }catch (Exception e){
+            logger.error(e.getMessage());
+        }
     }
 
 }
