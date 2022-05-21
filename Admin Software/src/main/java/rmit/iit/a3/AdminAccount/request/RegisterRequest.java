319a19bee075a6d498a10e0e7100c57115da642e
Jingxuan Feng <henryfeng24@pm.me>
2022-05-22 02:03:18 +1000

    Finished register feature

--- Admin Software/src/main/java/rmit/iit/a3/AdminAccount/request/RegisterRequest.java
+++ Admin Software/src/main/java/rmit/iit/a3/AdminAccount/request/RegisterRequest.java
@@ -1,10 +1,12 @@
 package rmit.iit.a3.AdminAccount.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class RegisterRequest {
     private String username;
     private String password;
